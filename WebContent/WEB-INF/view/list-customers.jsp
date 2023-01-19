3e60ec4fbed0782cac622d0e639107e6c00d1e1c
Rohan Arora <mac@macs-MacBook-Air.local>
2023-01-19 18:11:55 +0530

    Injected Customer DAO to Customer controller Sent data using model to the view page

--- WebContent/WEB-INF/view/list-customers.jsp
+++ WebContent/WEB-INF/view/list-customers.jsp
@@ -1,5 +1,4 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +6,32 @@
 <title>Insert title here</title>
 </head>
 <body>
-	<p>I am coming mother fuckers...</p>
+	<div id="wrapper">
+		<div id="content">
+			<!-- add out html table here -->
+			
+			<table>
+				<tr>
+					<th>First Name</th>
+					<th>Last Name</th>
+					<th>Email</th>
+				</tr>
+				
+				<c:forEach var="c" items="${customers}">
+					<tr>
+						<td>
+							${c.firstName}
+						</td>
+						<td>
+							${c.lastName}
+						</td>
+						<td>
+							${c.email}
+						</td>
+					</tr>
+				</c:forEach>
+			</table>
+		</div>
+	</div>
 </body>
 </html>
--- src/com/luv2code/springdemo/controller/CustomerController.java
+++ src/com/luv2code/springdemo/controller/CustomerController.java
@@ -1,15 +1,31 @@
 package com.luv2code.springdemo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.luv2code.springdemo.dao.CustomerDAO;
+import com.luv2code.springdemo.entity.Customer;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 	
+	// need to inject DAO into the controller(dependency of injection)
+	@Autowired
+	private CustomerDAO customerDAO;
+	
 	@RequestMapping("/list")
 	public String listCustomers(Model theModel) {
+		
+		// get customers from DAO
+		List<Customer> customers = customerDAO.getCustomers();
+		
+		// add those customers to the model
+		theModel.addAttribute("customers", customers);
 		
 		return "list-customers";
 	}
